e553962c41eabf42ffa60a45c56b4a5715ef0b46
David Delgado <davidd82@usc.edu>
2023-09-21 09:37:54 -0700

    Add files via upload

---
+++ vm_subscriber.py
@@ -0,0 +1,37 @@
+"""EE 250L Lab 04 Starter Code
+
+Run vm_subscriber.py in a separate terminal on your VM."""
+
+import paho.mqtt.client as mqtt
+import time
+
+#Custom callbacks need to be structured with three args like on_message()
+def custom_callback(client, userdata, message):
+    #the third argument is 'message' here unlike 'msg' in on_message 
+    print("custom_callback: " + message.topic + " " + "\"" + 
+        str(message.payload, "utf-8") + "\"")
+    print("custom_callback: message.payload is of type " + 
+          str(type(message.payload)))
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code "+str(rc))
+
+    #subscribe to the ultrasonic ranger topic here
+    client.subscribe("davidd82/ultrasonicRanger")
+    client.message_callback_add("davidd82/customCallback", custom_callback)
+
+#Default message callback. Please use custom callbacks.
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
+
+if __name__ == '__main__':
+    #this section is covered in publisher_and_subscriber_example.py
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host= "test.mosquitto.org", port= 1883, keepalive=60)
+    client.loop_start()
+
+    while True:
+        print("delete this line")
+        time.sleep(1)        
