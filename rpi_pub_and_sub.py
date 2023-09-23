6137b53041b0b3c119d6a3a5dfb34717d2c58749
David Delgado <davidd82@usc.edu>
2023-09-23 14:33:59 -0700

    test

--- rpi_pub_and_sub.py
+++ rpi_pub_and_sub.py
@@ -41,7 +41,7 @@ def on_message(client, userdata, msg):
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
-    client.on_message = lcd_status
+    client.on_message = on_message
     client.on_connect = on_connect
     client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
     client.loop_start()
