1694b6b16dab50682abf7fc30799801b4a152265
David Delgado <davidd82@usc.edu>
2023-09-23 11:39:58 -0700

    Added LED control

--- rpi_pub_and_sub.py
+++ rpi_pub_and_sub.py
@@ -4,11 +4,29 @@ Run rpi_pub_and_sub.py on your Raspberry Pi."""
 
 import paho.mqtt.client as mqtt
 import time
+from grovepi import *
+
+led = 4
+pinMode(led, "OUTPUT")
+
+def custom_callback(client, userdata, message):
+    #the third argument is 'message' here unlike 'msg' in on_message 
+    print("custom_callback: " + message.topic + " " + "\"" + 
+        str(message.payload, "utf-8") + "\"")
+    print("custom_callback: message.payload is of type " + 
+          str(type(message.payload)))
+    
+    if (str(message.payload, "utf-8") == "LED_ON"):
+        digitalWrite(led,1)
+    elif(str(message.payload, "utf-8") == "LED_OFF"):
+        digitalWrite(led,0)
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
     #subscribe to topics of interest here
+    client.subscribe("davidd82/led")
+    client.message_callback_add("davidd82/customCallback", custom_callback)
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
@@ -22,6 +40,7 @@ if __name__ == '__main__':
     client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
     client.loop_start()
 
+    time.sleep(1)
     while True:
         print("delete this line")
         time.sleep(1)
